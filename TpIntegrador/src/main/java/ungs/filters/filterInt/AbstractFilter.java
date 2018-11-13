e0a7c60ead0327cf34a3234b4f5621b32d65fa44
Jaime Quispe <jaime.quispe@despegar.com>
2018-11-13 07:10:57 -0300

    ajuste filter

--- TpIntegrador/src/main/java/ungs/filters/filterInt/AbstractFilter.java
+++ TpIntegrador/src/main/java/ungs/filters/filterInt/AbstractFilter.java
@@ -1,11 +1,12 @@
 package ungs.filters.filterInt;
 
 import com.google.common.collect.Lists;
+import org.apache.commons.collections4.CollectionUtils;
 import ungs.connectors.AbstractConnector;
 import ungs.model.Configuration;
 import java.util.List;
 
-public abstract class AbstractFilter<I,O> implements Filter<O> {
+public abstract class AbstractFilter<I,O> implements Filter<I,O> {
 
     protected AbstractConnector connector;
     protected Configuration configuration;
@@ -25,5 +26,17 @@ public abstract class AbstractFilter<I,O> implements Filter<O> {
 
     public void setListInput(List<I> listInput) {
         this.listInput = listInput;
+    }
+
+    public List<O> doApplyFilter() {
+        if (CollectionUtils.isNotEmpty(listInput)) {
+
+        }
+        return applyFilter();
+    }
+
+    @Override
+    public List<O> applyFilter(List<I> list) {
+        return null;
     }
 }
--- TpIntegrador/src/main/java/ungs/filters/filterInt/Filter.java
+++ TpIntegrador/src/main/java/ungs/filters/filterInt/Filter.java
@@ -2,8 +2,9 @@ package ungs.filters.filterInt;
 
 import java.util.List;
 
-public interface Filter<OUTPUT> {
+public interface Filter<INPUT, OUTPUT> {
 
     List<OUTPUT> applyFilter();
+    List<OUTPUT> applyFilter(List<INPUT> list);
 
 }
